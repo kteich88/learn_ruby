bbbe55d77845cb6d070972b9539d1fe42b5666e6
Kristine Teichmann <kteichmann@healthgrades.com>
2018-10-04 09:23:39 -0400

    passing tests

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,20 +1,7 @@
-def hello(name)
-    if name != ""
-        puts "Hello #{name}"
-    elsif name == "Kristine"
-        puts "Hello #{name}"
-    elsif name == "Romina"
-        puts "Hello Romina"
-    else
-        puts "Hello World!"
-    end
+def hello
+  "Hello!"
 end
 
-def main
-    print "What is your name? "
-    name = gets.chomp
-    hello(name)
+def greet(name)
+  "Hello, #{name}!"
 end
-
-
-main if __FILE__ ==$PROGRAM_NAME
